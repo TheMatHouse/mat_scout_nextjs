8fd11bcc76aaf4d2fe77ede1377610ff9824ffd5
judo2000 <judo2000@comcast.net>
2025-01-16 16:45:06 -0700

    Add redirect to dashbhard in layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { customMetaDataGenerator } from "@/lib/CustomMetaDataGenerator";
 import Header from "@/components/shared/Header";
+import { currentUser } from "@clerk/nextjs/server";
 import "react-toastify/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
@@ -9,13 +10,16 @@ import { ToastContainer } from "react-toastify";
 import SidebarMenuBar from "@/components/shared/sidebar/SidebarMenuBar";
 import { ClerkProvider, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 import Loader from "@/components/shared/Loader";
+import { redirect } from "next/navigation";
 
 export const metadata = customMetaDataGenerator({
   title: "MatScout.com - Premier Grappling Site for Athlete Scouting",
 });
 
 export default async function RootLayout({ children }) {
-  //const session = await auth();
+  const user = await currentUser();
+  console.log("USER ", user);
+  if (user) redirect("/dashboard");
   return (
     <ClerkProvider>
       <html
