e6a8910f9768ba9f2edcd410c1679e09a9441289
judo2000 <judo2000@comcast.net>
2025-05-30 13:23:10 -0600

    Add token check to dashboard/page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,12 @@
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default function DashboardIndex() {
+  const token = cookies().get("token");
+
+  if (!token) {
+    redirect("/");
+  }
+
   redirect("/dashboard/settings");
 }
