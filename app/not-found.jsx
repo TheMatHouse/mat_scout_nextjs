d60a0c51b5098d26b4668ac156d98e050a333706
judo2000 <judo2000@comcast.net>
2025-08-05 19:13:29 -0600

    Add use-dynamie to not-found page

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center px-6 bg-white dark:bg-gray-900">
