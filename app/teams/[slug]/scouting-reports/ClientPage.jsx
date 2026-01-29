7f6b952a0ce3c2ef299e5676398fbf3d2ffd0f84
judo2000 <judo2000@comcast.net>
2026-01-29 16:38:27 -0700

    Fix componet import in ClientPage.jsx

--- app/teams/[slug]/scouting-reports/ClientPage.jsx
+++ app/teams/[slug]/scouting-reports/ClientPage.jsx
@@ -12,7 +12,7 @@ import ModalLayout from "@/components/shared/ModalLayout";
 import Spinner from "@/components/shared/Spinner";
 import TeamUnlockGate from "@/components/teams/TeamUnlockGate";
 import ScoutingReportCard from "@/components/shared/ScoutingReportCard";
-import MatScoutPagination from "@/components/shared/MatcoutPagination";
+import MatScoutPagination from "@/components/shared/MatscoutPagination";
 
 import { decryptScoutingBody } from "@/lib/crypto/teamLock";
 import { canView, canEdit, canDelete, canCreate } from "./logic/roleUtils";
